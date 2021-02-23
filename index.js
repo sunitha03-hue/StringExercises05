ef3c1486228f90b2075a08c757249b5442dab811
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-02-23 21:22:23 +0000

    Java Exercise 7.9.3 Part 3 first completed

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let language = 'JavaScript';
 
 //a) Use string concatenation and two .slice() methods to print 'JS' from 'JavaScript'.
-
-
+let language1 =language.slice(0, 1)+ language.slice(4,5);
+console.log(language1);
 
 //b) Without using .slice(), use method chaining to accomplish the same thing.
 
-
-
+let language2=language.charAt(0)+language.charAt(4);
+console.log(language2);
 //c) Use bracket notation and a template literal to print "The abbreviation for 'JavaScript' is 'JS'."
 
+let language3=language[0]+language[4];
+console.log(`The abbreviation for, ${language} is ${language3}`);
 
 
 //d) Just for fun, try chaining 3 or more methods together, and then print the result.
+let language4=language.slice(0,1)+language.charAt(4);
+console.log(language4);
